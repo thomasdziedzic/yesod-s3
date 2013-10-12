00b6ea1c709f8267546e9d70041a5d6d10035f72
Timo von Holtz <tvh@tvholtz.de>
2013-10-12 14:51:17 +0200

    add new function: getLink

--- Yesod/S3.hs
+++ Yesod/S3.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Yesod.S3 where
 
 import Data.ByteString.Lazy
 import Yesod.Core.Types
 import Data.Conduit
 import Data.Conduit.Binary
+import Network.URI
 import Network.AWS.AWSConnection
 import Network.AWS.AWSResult
 import Network.AWS.S3Object
@@ -21,3 +23,11 @@ uploadFile conn fi bucket name = do
   bs <- extractFile fi
   let obj = S3Object bucket name (T.unpack $ fileContentType fi) [] bs
   sendObjectMIC conn obj
+
+getLink :: AWSConnection
+        -> String -- ^ The Bucket Name
+        -> String -- ^ The Object Name
+        -> IO URI -- ^ IO is only used to get the current Time
+getLink conn bucket name = do
+  let obj = S3Object bucket name "" [] ""
+  publicUriForSeconds conn obj 60
